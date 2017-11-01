5b57298013f54aad4e0068eb5bb0725c5cad063b
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-11-01 10:10:18 +0100

    calculate center index

--- simple-noise-rs/src/lib.rs
+++ simple-noise-rs/src/lib.rs
@@ -20,6 +20,32 @@ pub fn generate_map(
         width * height
     ];
 
+    let center_index = get_center_node_index(
+        width,
+        height,
+    );
+
     // TODO: added here just for the interface, must be defined
     map
+}
+
+/// Returns the index of the center node of the map.
+///
+/// # Args:
+///
+/// `width` - the map width
+/// `height` - the map height
+///
+/// # Returns:
+///
+/// The index of the center.
+fn get_center_node_index(
+    width: usize,
+    height: usize,
+) -> usize {
+
+    let horizontal_index = width / 2;
+    let vertical_index = height / 2;
+
+    vertical_index * width + horizontal_index
 }
