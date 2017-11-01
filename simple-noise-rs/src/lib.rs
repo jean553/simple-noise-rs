317349c601ac690c5cc2ebf530ee39a004b8d4b0
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-11-01 10:47:43 +0100

    create test in order to check the first value generation

--- simple-noise-rs/src/lib.rs
+++ simple-noise-rs/src/lib.rs
@@ -16,19 +16,67 @@ use rand::{
 ///
 /// Vector with the map nodes values.
 ///
-/// TODO: Should return a Vec<u8>, the option is needed for the map generation.
+/// TODO: Should return a Vec<i8>, the option is needed for the map generation.
 pub fn generate_map(
     width: usize,
     height: usize,
-) -> Vec<Option<u8>> {
+    minimum: i8,
+    maximum: i8,
+) -> Vec<Option<i8>> {
 
     let mut map = vec![
         None;
         width * height
     ];
 
-    map[0] = Some(rand::random::<u8>());
+    let mut random_number_generator = thread_rng();
+
+    map[0] = Some(
+        random_number_generator.gen_range(
+            minimum,
+            maximum,
+        )
+    );
 
     // TODO: added here just for the interface, must be defined
     map
+}
+
+#[cfg(test)]
+mod tests {
+
+    use generate_map;
+
+    #[test]
+    fn test_first_index_random_generation() {
+
+        const WIDTH: usize = 10;
+        const HEIGHT: usize = 10;
+        const MINIMUM: i8 = -10;
+        const MAXIMUM: i8 = 10;
+        let values = generate_map(
+            WIDTH,
+            HEIGHT,
+            MINIMUM,
+            MAXIMUM,
+        );
+
+        let first_value = values[0].unwrap();
+
+        assert!(
+            first_value > -10,
+            format!(
+                "The first node {} value is too small.",
+                first_value,
+            )
+        );
+
+        assert!(
+            first_value < 10,
+            format!(
+                "The first node {} value is too high.",
+                first_value,
+            )
+        );
+    }
 }
